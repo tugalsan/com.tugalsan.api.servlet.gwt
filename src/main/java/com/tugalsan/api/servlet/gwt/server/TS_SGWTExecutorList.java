3fcb6a60c513e282dc8894c57be126cd0aaa9b2b
tugalsan karabacak <tugalsan@gmail.com>
2023-08-03 20:30:17 +0300

    Update TS_SGWTExecutorList.java

--- src/main/java/com/tugalsan/api/servlet/gwt/server/TS_SGWTExecutorList.java
+++ src/main/java/com/tugalsan/api/servlet/gwt/server/TS_SGWTExecutorList.java
@@ -2,14 +2,14 @@ package com.tugalsan.api.servlet.gwt.server;
 
 import java.util.*;
 import com.tugalsan.api.log.server.*;
-import com.tugalsan.api.thread.server.safe.TS_ThreadSafeLst;
+import com.tugalsan.api.thread.server.sync.TS_ThreadSyncLst;
 import com.tugalsan.api.tuple.client.*;
 
 public class TS_SGWTExecutorList {
 
     final private static TS_Log d = TS_Log.of(TS_SGWTExecutorList.class);
 
-    public static TS_ThreadSafeLst<TGS_Tuple2<String, TS_SGWTExecutor>> SYNC = new TS_ThreadSafeLst();
+    public static TS_ThreadSyncLst<TGS_Tuple2<String, TS_SGWTExecutor>> SYNC = new TS_ThreadSyncLst();
 
     public static TS_SGWTExecutor add(TS_SGWTExecutor exe) {
         SYNC.add(new TGS_Tuple2(exe.name(), exe));

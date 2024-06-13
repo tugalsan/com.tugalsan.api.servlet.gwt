dac7b4872ab92a07d44590f3488a97026aecda59
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-13 03:14:30 +0300

    Update TGC_SGWTResponse.java

--- src/main/java/com/tugalsan/api/servlet/gwt/client/TGC_SGWTResponse.java
+++ src/main/java/com/tugalsan/api/servlet/gwt/client/TGC_SGWTResponse.java
@@ -82,7 +82,7 @@ public class TGC_SGWTResponse<T extends TGS_SGWTFuncBase> implements AsyncCallba
             d.ci("onSuccess", "#6");
             var errMsg = funcBase.getExceptionMessage();
             if (errMsg.contains("cannot run (validate)")) {
-                errMsg = errMsg + " [UYARI: Yetkilendirilmiş gün sayısından daha önceki bir kayıtta değişiklik yapılmaya çalışılmış olabilir.]";
+                errMsg = "[UYARI: Yetkilendirilmiş gün sayısından daha önceki bir kayıtta değişiklik yapılmaya çalışılmış olabilir.]" + errMsg;
             }
             d.ci("onSuccess", "#7");
             onFailure(TGS_UnSafe.toRuntimeException(d.className, "onSuccess", "ERROR: onSuccess -> getMessage: " + errMsg));
